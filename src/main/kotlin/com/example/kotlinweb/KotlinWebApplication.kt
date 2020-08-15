556ca43fe7a8def6231da05a90ba218bbaac4318
zero-dev <zero.dev@kakaoenterprise.com>
2020-08-15 18:13:55 +0900

    [Refactoring] Application Class 카멜케이스 적용

--- src/main/kotlin/com/example/kotlinweb/KotlinWebApplication.kt
+++ src/main/kotlin/com/example/kotlinweb/KotlinWebApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinwebApplication
+class KotlinWebApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinwebApplication>(*args)
+    runApplication<KotlinWebApplication>(*args)
 }
