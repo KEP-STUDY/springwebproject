25092599c318233ed0c5209b59cf50c8d0f32242
harry(조민국) <harry.code@kakaoenterprise.com>
2020-08-20 20:05:22 +0900

    init

---
+++ src/test/kotlin/me/harry/study/StudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.harry.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
