16aa5d637d3b9020e3f67df1db10576983d3a18e
mark.kang <mark.kang@kakaoenterprise.com>
2020-10-06 17:56:48 +0900

    TestCode 수정

--- src/test/kotlin/com/example/kotlinweb/board/service/BoardServiceImplTest.kt
+++ src/test/kotlin/com/example/kotlinweb/board/service/BoardServiceImplTest.kt
@@ -18,11 +18,11 @@ internal class BoardServiceImplTest(
     @Test
     @Transactional
     fun savePostTest() {
-        val post: Post = Post(1L, "Test Title", "KDH", "memo")
+        val post = Post(1L, "Test Title", "KDH", "memo")
         val boardService: BoardService = BoardServiceImpl(repository)
         boardService.savePost(post)
         val savedPost: Post? = boardService.findPostById(1L)
-        savedPost.let {
+        savedPost?.let {
             Assertions.assertEquals(post.title, savedPost.title)
             assertThat(post.title).isEqualTo(savedPost.title)
         }
@@ -31,15 +31,15 @@ internal class BoardServiceImplTest(
     @Test
     @Transactional
     fun readPostsTest() {
-        val post1: Post = Post(1L, "Test Title", "KDH", "memo1")
-        val post2: Post = Post(2L, "Test Title2", "KDH2", "memo2")
+        val post1 = Post(1L, "Test Title", "KDH", "memo1")
+        val post2 = Post(2L, "Test Title2", "KDH2", "memo2")
         val boardService: BoardService = BoardServiceImpl(repository)
         boardService.savePost(post1)
         boardService.savePost(post2)
 
         val posts: MutableList<Post> = boardService.findPosts()
-        val savedPost1: Post = posts[0] as Post
-        val savedPost2: Post = posts[1] as Post
+        val savedPost1: Post = posts[0]
+        val savedPost2: Post = posts[1]
         //assertion
         assertAll(
             "posts",
@@ -56,7 +56,7 @@ internal class BoardServiceImplTest(
     @Test
     @Transactional
     fun increaseHitCount() {
-        val post1: Post = Post(1L, "Test Title", "KDH", "memo1")
+        val post1 = Post(1L, "Test Title", "KDH", "memo1")
         post1.increaseHitCount()
         //assertion
         Assertions.assertEquals(post1.hitCount, 1)
